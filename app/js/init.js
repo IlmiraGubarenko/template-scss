ebdc3fb0450b5a541cd3781bc090d880b2086340
ilmira <you@example.com>
2017-09-29 16:55:37 +0300

    добавлен календарь на нужные страницы, добавлено модальное окно

--- app/js/init.js
+++ app/js/init.js
@@ -67,4 +67,11 @@ $( function() {
         }
     });
 
+    // modal init
+    var modalBtn = $('.links .btn');
+
+    modalBtn.on('click', function (event) {
+        event.preventDefault();
+        $('.modal').bPopup();
+    });
 } );
